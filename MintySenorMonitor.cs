bd548074463fd080a1401a178d91b34aeeace253
Rob Gregory [Minty] <MintyMods@gmail.com>
2019-06-01 23:50:54 +0100

    removed documentation from class into readme.md

--- MintySenorMonitor.cs
+++ MintySenorMonitor.cs
@@ -4,16 +4,6 @@ using System.Diagnostics;
 
 namespace com.mintymods.msm
 {
-	/***************************************************************************************************************
-	 * MSM (MintySensorMonitor) is the integration between HWiNFO and MSS (MintySensorServer)
-	 * SENSORS <-?-> HWiNFO <-?-> MSM[C#:SHM] <--> MSS[JNI:JSON] <--> API[JAVA:REST/JSOM/HTML]
-	 * This is done using C# <-JSON-> JNI <--> JAVA <--> HTTP(S) <--> HTML5
-	 * For more information see the following projects:-
-	 * MSM:https://github.com/MintyMods/MintySM
-	 * MSS:https://github.com/MintyMods/MintySS
-	 * MSQ:https://github.com/MintyMods/MintySQ
-	/***************************************************************************************************************/
-	
 	public class MintySenorMonitor
 	{
 		public string getSensorInfoAsJSON()
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MintySM
-MSM (Minty Sensor Monitor) is the integration between HWiNFO and MSS (Minty's Sensor Server)
+
+	/***************************************************************************************************************
+	 * MSM (MintySensorMonitor) is the integration between HWiNFO and MSS (MintySensorServer)
+	 * SENSORS <-?-> HWiNFO <-?-> MSM[C#:SHM] <--> MSS[JNI:JSON] <--> API[JAVA:REST/JSOM/HTML]
+	 * This is done using C# <-JSON-> JNI <--> JAVA <--> HTTP(S) <--> HTML5
+	 * For more information see the following projects:-
+	 * MSM:https://github.com/MintyMods/MintySM
+	 * MSS:https://github.com/MintyMods/MintySS
+	 * MSQ:https://github.com/MintyMods/MintySQ
+	/***************************************************************************************************************/
